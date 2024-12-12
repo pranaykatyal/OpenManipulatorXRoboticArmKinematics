91f8ace4940935446678dbaadaa3b76f6374c662
Quinn Ferguson <quinnferguson48@gmail.com>
2024-12-12 18:55:25 -0500

    complete joint controller node

--- open_mx/src/robot_omx/robot_omx/joint_controller.py
+++ open_mx/src/robot_omx/robot_omx/joint_controller.py
@@ -1,57 +1,60 @@
 import rclpy
-from robot_omx import kinematic_library
 from rclpy.node import Node
-from std_msgs.msg import Float32MultiArray # This standard message type is used to receive the 3 parameters as an array.
-from geometry_msgs.msg import Pose
-from sensor_msgs.msg import JointState # We will need to test this in person
+from custom_messages.srv import SetTarget
+from dynamixel_sdk_custom_interfaces.srv import GetPosition
 from dynamixel_sdk_custom_interfaces.msg import SetCurrent
+
+interval = 0.01
 
 class JointController(Node):
 
-    def __init__(self, target):
+    def __init__(self,target):
         super().__init__("joint_controller")
 
         self.kp = 10
         self.kd = 10
         self.joint = 4
-        self.joint_val = 0
-        self.joint_delta =0
+        self.error_delta =0
+        self.error = 0
         self.target = target
 
-        self.refrence_srv = self.create_service()
-        self.joint_val_cli = self.create_client()
+        self.refrence_srv = self.create_service(SetTarget, 'pd_server', self.listener_callback)
+        self.joint_val_cli = self.create_client(GetPosition, 'get_position')
+        self.current_pub = self.create_publisher(SetCurrent, 'set_current', 10)
+        self.timer = self.create_timer(interval, self.timer_callback)
 
 
-    def listener_callback(self, msg):
-        interval = 0.1
-        thisval = msg.position[self.joint-1]
+    def listener_callback(self, request, response):
+        self.target = request.target
 
-        self.joint_delta = (self.joint_val - thisval)/interval
-        self.joint_val = thisval
+    def timer_callback(self):
+        
+        # Create and send request to get current position of joint
+        req = GetPosition.Request() 
+        req.id = 14
+        pos = self.joint_val_cli.call_async(req)
+        rclpy.spin_until_future_complete(self, pos) # Ensures program waits for a result prior to printing to the terminal.
+
+        # Calculate error between curr pos and target
+        thiserror = self.target - pos
+        
+        self.error_delta = (self.error - thiserror)/interval
+        self.error = thiserror
 
         effort = self.kp*(self.target-self.joint_val) + self.kd*self.joint_delta
 
-        #TODO: set effort
+        # Create message with the new effort value and publish it to the current sub
+        thismsg = SetCurrent()
+        thismsg.id = 14
+        thismsg.current = effort
+        self.current_pub.publish(effort)
 
-
-# The below class creates a node that sends a pose to a server. The server then returns the joint parameters for that pose.
-class Control_Publisher(Node):
-
-    def __init__(self):
-        # Creating a client that sends a desired pose to inverse_server
-        super().__init__('control_publisher')
-        self.publisher = self.create_publisher(SetCurrent, 'control_values', 10)  # (Type, name)
-
-    def control_client_request(self, id, current):
-        self.get_logger().info(f'The current value is {current}, of type {type(current)}') # Posting the result to the terminal
-        self.publisher.publish(id, current)
 
 
 
 def main():
     rclpy.init()
-    control_publisher = Control_Publisher()  # initializing the publisher object
-    control_publisher.control_client_request(14, 1.0)
-    rclpy.spin(control_publisher)  # Running the node continously
+    jc = JointController(1000)
+    rclpy.spin(jc)  # Running the node continously
 
     
